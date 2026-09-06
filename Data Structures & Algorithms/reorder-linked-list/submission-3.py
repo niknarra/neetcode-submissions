323bbfb8d9d8dbce7fc5fbf1df00b4c14a5dd1e3
Nikhil Narra <126026225+niknarra@users.noreply.github.com>
2026-09-06 14:05:23 -0500

    Add: reorder-linked-list - submission-3

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-3.py
@@ -0,0 +1,36 @@
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        slow = fast = head
+
+        while fast and fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        def reverso(head):
+            curr = head
+            prev = None
+
+            while curr:
+                fwd = curr.next
+                curr.next = prev
+                prev = curr
+                curr = fwd
+            
+            return prev
+        
+        # Find the end of the first half → detach the two halves → reverse the second half → 
+        # weave the two lists together.
+        rev = reverso(slow.next)
+        slow.next = None
+
+        curr = head
+
+        while rev:
+            fwd = curr.next
+            revNext = rev.next
+            
+            curr.next = rev
+            rev.next = fwd
+
+            curr = fwd
+            rev = revNext
